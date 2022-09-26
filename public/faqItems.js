34c308172f802ba8463fb5363e2f6b02e8d6c7f4
Your Name <you@example.com>
2022-09-26 18:41:59 +0300

    move mint date

--- public/faqItems.js
+++ public/faqItems.js
@@ -5,7 +5,7 @@ const faqItems = [
         number: '01'
     },
     {
-        text: `September 8: 
+        text: `September 29: 
         11:00 EDT Allowlist: First FREE, second — 0.005 ETH.
         13:00 EDT Waitlist: 0.005 ETH per nft.
         13:30 EDT Waitlist: 0.003 ETH per nft.
